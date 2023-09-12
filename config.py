b38684e8da6ddeee44f744229708e73d64938312
LeeSY0110 <112144874+LeeSY0110@users.noreply.github.com>
2023-09-12 21:18:49 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-customhost = "internship.cpc3sv6dkw4o.us-east-1.rds.amazonaws.com"
+customhost = "companydb.cpc3sv6dkw4o.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
-customdb = "internship"
+customdb = "companydb"
 custombucket = "sy-internship"
 customregion = "us-east-1"
 
